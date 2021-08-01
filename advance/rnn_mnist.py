b5c25e633aacf8b233f0b47928863dc54c18f861
Bill Ma <mazengou@gmail.com>
2021-08-01 11:49:01 +0800

    Implement RNN(LSTM)

--- advance/rnn_mnist.py
+++ advance/rnn_mnist.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.utils.data as Data
+import torchvision.transforms as transforms
 import torchvision
 import matplotlib.pyplot as plt
 
@@ -17,7 +19,7 @@ DOWNLOAD_MNIST = False
 train_data = torchvision.datasets.MNIST(
     root="../data/mnist/",
     train=True,
-    transform=torchvision.transforms.ToTensor(),
+    transform=transforms.ToTensor(),
     download=DOWNLOAD_MNIST
 )
 
@@ -26,17 +28,14 @@ train_data = torchvision.datasets.MNIST(
 # plt.imshow(train_data.data[0].numpy(), cmap="gray")
 # plt.title("%i" % train_data.targets[0])
 # plt.show()
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
-test_data = torchvision.datasets.MNIST(root="../data/mnist/", train=False)
-train_loader = Data.DataLoader(
-    dataset=train_data,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-)
 # Only take first 2000 samples for testing
 # (2000, 1, 28, 28), value in range(0,1)
-test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:2000]/255.
+test_data = torchvision.datasets.MNIST(root="../data/mnist/", train=False, transform=transforms.ToTensor())
+test_x = test_data.data.type(torch.FloatTensor)[:2000]/255.
 test_y = test_data.targets[:2000]
+# print(test_x.data.size())
 
 class RNN(nn.Module):
     def __init__(self):
@@ -50,6 +49,41 @@ class RNN(nn.Module):
         self.out = nn.Linear(64, 10)
 
     def forward(self, x):
+        # x shape (batch, time_step, input_size)
+        # r_out shape (batch, time_step, output_size)
+        # h_n shape (n_layers, batch, hidden_size)
+        # h_c shape (n_layers, batch, hidden_size)
         r_out, (h_n, h_c) = self.rnn(x, None)
         out = self.out(r_out[:, -1, :])
         return out
+
+rnn = RNN()
+# print(rnn)
+"""
+RNN(
+  (rnn): LSTM(28, 64, batch_first=True)
+  (out): Linear(in_features=64, out_features=10, bias=True)
+)
+"""
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()
+
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):
+        b_x = b_x.view(-1, 28, 28)
+        output = rnn(b_x)
+        loss = loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % 50 == 0:
+            test_output = rnn(test_x)
+            pred_y = torch.max(test_output, 1)[1]
+            accuracy = float((pred_y == test_y).sum().data.numpy()) / float(test_y.size()[0])
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+
+test_output = rnn(test_x[:10].view(-1, 28, 28))
+pred_y = torch.max(test_output, 1)[1].data.numpy()
+print(pred_y, "Prediction Number")
+print(test_y[:10].data.numpy(), "Ground Truth")
